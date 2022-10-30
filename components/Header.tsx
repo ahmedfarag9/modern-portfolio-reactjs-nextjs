116a7fe3274c74f057116f950477089a9c126a4a
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-10-30 10:52:34 +0200

    Add Social-icon element

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,6 +22,15 @@ export default function Header({}: Props) {
             duration: 1.5
         }}
         className='flex flex-center items-center text-gray-300 cursor-pointer'>
+        <SocialIcon 
+            className='cursor-pointer'
+            network='email' 
+            fgColor='gray'
+            bgColor='transparent'
+            />
+            <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
+        </motion.div>
 
+    </header>
   )
 }
