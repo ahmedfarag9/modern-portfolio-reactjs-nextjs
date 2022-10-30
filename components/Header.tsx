f9a56289f57b3a071f8ef113682b1d5e85bb8f23
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-10-30 10:53:23 +0200

    Add motion dev

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,23 @@ type Props = {}
 
 export default function Header({}: Props) {
   return (
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+        <motion.div
+            initial={{
+                x: -500,
+                opacity: 0,
+                scale: 0.5
+            }}
+            animate={{
+                x: 0,
+                opacity: 1,
+                scale: 1
+            }}
+            transition={{
+                duration: 1.5
+            }}
+        </motion.div>
+
         <motion.div 
         initial={{
             x: 500,
