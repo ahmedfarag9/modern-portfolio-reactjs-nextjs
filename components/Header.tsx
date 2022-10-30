f073572fa85c8f916d973e1e0e5124a25c9c8dc0
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-10-30 10:51:29 +0200

    Add motion div

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,21 @@ type Props = {}
 
 export default function Header({}: Props) {
   return (
+        <motion.div 
+        initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5,
+        }}
+        animate={{
+            x:0,
+            opacity: 1,
+            scale: 1,
+        }}
+        transition={{
+            duration: 1.5
+        }}
+        className='flex flex-center items-center text-gray-300 cursor-pointer'>
 
   )
 }
