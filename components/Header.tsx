e6b4635965af9cc0c3a26c288fc527a9e54a8a78
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-10-30 10:50:29 +0200

    Add default func

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,3 +2,11 @@ import React from 'react'
 import { SocialIcon } from 'react-social-icons';
 import { motion } from "framer-motion"
 
+
+type Props = {}
+
+export default function Header({}: Props) {
+  return (
+
+  )
+}
