d03a239365d9f3075da17a9bf55d0c31cbd880a7
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-02 13:01:01 +0200

    Add About.tsx

---
+++ components/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { motion } from "framer-motion"
+import profilePic from '../public/image.jpg'
+
+
+type Props = {}
+
+function About({}: Props) {
+  return (
+
+  )
+}
+
+export default About
