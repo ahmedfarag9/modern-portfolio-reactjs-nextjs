912aa6fd8230c7189ee825986546b74aa0cc344d
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-02 13:03:23 +0200

    Add Animation

--- components/About.tsx
+++ components/About.tsx
@@ -20,6 +20,19 @@ function About({}: Props) {
 
         <motion.img
         initial={{
+            x: -200,
+            opacity: 0
+        }}
+        transition={{
+            duration: 1.2,
+        }}
+        whileInView={{opacity: 1, x: 0}}
+        viewport={{once: true}}
+        src='https://www.kibrispdr.org/data/30/clip-art-men-1.jpg'
+        alt='user picture'
+        className='mb-20 md:mb-0 flex-shrink-0 w-56 h-56
+        rounded-full object-cover md:rounded-lg md:w-64 md:h-95
+        xl:w-[500px] xl:h-[600px] mt-20 '
         />
         <div className='space-y-10 px-0 md:px-10'>
             <h4 className='text-4xl font-semibold'>
