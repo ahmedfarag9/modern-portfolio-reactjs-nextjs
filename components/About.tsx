e4dd53ef58df448fce882af066485012b1e7ba46
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-02 13:03:04 +0200

    Add motion.img component

--- components/About.tsx
+++ components/About.tsx
@@ -18,6 +18,19 @@ function About({}: Props) {
             About
         </h3>
 
+        <motion.img
+        initial={{
+        />
+        <div className='space-y-10 px-0 md:px-10'>
+            <h4 className='text-4xl font-semibold'>
+                Here is a <span className='underline decoration-[#F7AB0A]/50'>
+                little</span>{" "}background
+            </h4>
+            <p className='text-base'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+        
     </motion.div>
   )
 }
