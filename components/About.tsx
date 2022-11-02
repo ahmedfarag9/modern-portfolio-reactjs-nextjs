e816566f32c0f3ad59be51152bc600bbf3742323
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-02 13:01:44 +0200

    Add motion.div

--- components/About.tsx
+++ components/About.tsx
@@ -7,7 +7,18 @@ type Props = {}
 
 function About({}: Props) {
   return (
+    <motion.div
+    initial={{ opacity: 0 }}
+    transition={{ duration: 1 }}
+    whileInView={{opacity: 1}}
+    className=' flex flex-col relative h-screen text-center md:text-left
+    md:flex-row max-w-7xl px-10 justify-evenly items-center'>
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500
+        text-2xl'>
+            About
+        </h3>
 
+    </motion.div>
   )
 }
 
