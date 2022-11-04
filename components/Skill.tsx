d52cb0265f65b0baa3481240ca8bf3a608572c46
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-04 09:16:15 +0200

    Add motion.img component

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -18,6 +18,16 @@ function Skill({ directionLeft }: Props) {
             opacity: 1,
             x: 0
         }}
+        src='https://reactjs.org/logo-og.png'
+        className='rounded-full border border-grey-500 object-cover w-24 h-24 md:h-28 md:w-28 xl:w-32 xl:h-32
+        filter group-hover:grayscale transition duration-300 ease-in-out'
+        />
+        <div className='absolute opacity-0 group-hover:opacity-60 transition duration-300
+        ease-in-out group-hover:bg-white h-24 w-24 md:h-28 md:w-28 xl:h-32 xl:w-32 rounded-full z-0'>
+            <div className='flex items-center justify-center h-full'>
+                <p className='text-3xl font-bold opacity-100 text-black '>100%</p>
+            </div>
+        </div>
     </div>
   )
 }
