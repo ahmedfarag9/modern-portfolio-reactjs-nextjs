714364ff9c158480491b4ade1ec378f950af95f1
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-04 09:12:08 +0200

    Add Skills.tsx

---
+++ components/Skills.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { motion } from "framer-motion"
+import Skill from './Skill'
+
+type Props = {}
+
+function Skills({}: Props) {
+  return (
+  )
+}
+
+export default Skills
