a984e906de1de484ddfc4f7d5d1a22dcba0e0ed7
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-12 20:19:33 +0200

    Add Animation Style

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -25,6 +25,19 @@ function Projects({}: Props) {
         {projects.map((project, i) => (
           <div className='w-screen flex-shrink-0 snap-center flex-col space-y-5 items-center
           justify-center p-20 md:p-44 h-screen'>
+          <motion.img
+            initial={{ y: -300, opacity: 0 }}
+            transition={{ duration: 1.2 }}
+            whileInView={{opacity: 1, y: 0}}
+            viewport={{ once: true }}
+            src="https://st4.depositphotos.com/21230196/23977/v/450/depositphotos_239776244-stock-illustration-specialist-icon-vector-white-background.jpg"
+            alt="" 
+            />
+            <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
+              <h4 className='text-4xl font-semibold text-center'>
+                <span className='underline decoration-[#F&AB01]/50'> Case Study {i+1} of {projects.length}:</span>   UPS clone
+              </h4>
+
               <p className='text-lg text-center md:text-left'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
