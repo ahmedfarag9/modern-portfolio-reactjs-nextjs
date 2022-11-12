7623e5e9eadbd940dc8797dc2836ad6ded1aac62
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-12 20:19:58 +0200

    Add new Imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import Head from 'next/head'
+import About from '../components/About'
+import ContactMe from '../components/ContactMe'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
-import styles from '../styles/Home.module.css'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
+import WorkExperience from '../components/WorkExperience'
+
 
 export default function Home() {
   return (
