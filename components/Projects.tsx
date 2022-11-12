2f39bc30eedd2c5b99f5a5d3f2f259ee784e9d49
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-12 20:18:50 +0200

    Add Dummy Text

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -19,6 +19,17 @@ function Projects({}: Props) {
           Projects
       </h3>
 
+              <p className='text-lg text-center md:text-left'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className='w-full absolute top-[30%] bg-[#F7AB01]/10 left-0 h-[500px]
+      -skew-y-12 '/>
     </motion.div>
   )
 }
