9b55365bb5471cf68c15a03d619b894b5316fd3b
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-12 20:20:14 +0200

    Edit style

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,9 @@ import WorkExperience from '../components/WorkExperience'
 
 export default function Home() {
   return (
-    <div className='bg-[#363636] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
+    <div className='bg-[#363636] text-white h-screen snap-y snap-mandatory
+    overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20
+     scrollbar-thumb-[#F7AB0A]/80'>
       <Head>
         <title>Ahmed's Portfolio</title>
       </Head>
