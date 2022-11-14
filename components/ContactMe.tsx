a3585aec3415828cbc8103c215375d5e01bd0220
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-15 01:34:40 +0200

    Add main div

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -24,7 +24,16 @@ function ContactMe({}: Props) {
     }
 
   return (
+    <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl
+    px-10 justify-evenly mx-auto items-center'>
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500
+        text-2xl'>
+            Contact
+        </h3>
 
+
+
+   </div>
   )
 }
 
