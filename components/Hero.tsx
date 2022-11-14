b18167f197b7566e748c7d5ca5892af841529647
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-15 01:38:51 +0200

    Code refactor

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,13 +29,13 @@ export default function Hero({}: Props) {
         src={profilePic}
         alt='user profile picture'
       />
-
+      
       <div className='z-20'>
         <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer</h2>
 
         <h1 className='text-3xl lg:text-6xl font-semibold px-10'>
-          <span>{text}</span>
-          <Cursor cursorColor='#F7AB0A'/>
+            <span>{text}</span>
+            <Cursor cursorColor='#F7AB0A'/>
         </h1>
 
         <div className='pt-5'>
