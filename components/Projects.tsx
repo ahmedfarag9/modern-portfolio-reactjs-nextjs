2b340ef1eba5a878e1c663bb0db14c102e4843c5
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-14 22:57:53 +0200

    Add key to the map func

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -15,15 +15,15 @@ function Projects({}: Props) {
     className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row
     max-w-full justify-evenly mx-auto items-center z-0'>
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500
-        text-2xl'>
-          Projects
+      text-2xl'>
+        Projects
       </h3>
 
       <div className='relative w-full flex overflow-scroll overflow-y-hidden snap-x snap-mandatory
       z-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80
       '>
         {projects.map((project, i) => (
-          <div className='w-screen flex-shrink-0 snap-center flex-col space-y-5 items-center
+          <div key={i} className='w-screen flex-shrink-0 snap-center flex-col space-y-5 items-center
           justify-center p-20 md:p-44 h-screen'>
           <motion.img
             initial={{ y: -300, opacity: 0 }}
