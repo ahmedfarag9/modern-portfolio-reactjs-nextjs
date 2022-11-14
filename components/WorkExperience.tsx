8f6e264d7ac738ac9827165f9f1be4dd0c052986
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-15 01:40:58 +0200

    Add ExperienceCard elements

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -19,6 +19,12 @@ function WorkExperience({}: Props) {
         Experience
         </h3>
     
+    <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory
+    scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
+        <ExperienceCard/>
+        <ExperienceCard/>
+        <ExperienceCard/>
+    </div>
 
     </motion.div>
   )
