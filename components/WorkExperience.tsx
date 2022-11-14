cf47160e9a5706add65bfcd3e181b362961d1cb1
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-15 01:39:48 +0200

    Add WorkExperience.tsx

---
+++ components/WorkExperience.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { motion } from "framer-motion"
+import ExperienceCard from './ExperienceCard'
+
+
+type Props = {}
+
+function WorkExperience({}: Props) {
+  return (
+
+  )
+}
+
+export default WorkExperience
