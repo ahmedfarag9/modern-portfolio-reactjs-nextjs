8adabf47d4b79883b1ead6fe3cb33741b860b29e
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-15 01:31:38 +0200

    Add ContactMe.tsx

---
+++ components/ContactMe.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid'
+import Projects from './Projects'
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Props = {}
+
+function ContactMe({}: Props) {
+
+  return (
+
+  )
+}
+
+export default ContactMe
