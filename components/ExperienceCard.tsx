559759b35c3adac6836fba3718217badae076ebb
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-15 01:46:46 +0200

    Add article element

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -5,6 +5,11 @@ type Props = {}
 
 export default function ExperienceCard({}: Props) {
     return (
+        <article className='flex flex-col rounded-lg items-center space-y-7  flex-shrink-0
+        w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929 p-10] hover:opacity-100
+        opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+            <motion.img
 
+        </article>
     )
 }
