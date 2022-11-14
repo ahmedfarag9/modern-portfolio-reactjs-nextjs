a836c3bea3e3a04827db48337a73b08672f6e9ed
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-15 01:47:05 +0200

    Add Animation

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -9,6 +9,20 @@ export default function ExperienceCard({}: Props) {
         w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929 p-10] hover:opacity-100
         opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
             <motion.img
+            initial={{ 
+                y: -100,
+                opacity: 0 
+            }}
+            transition={{ duration: 1.2 }}
+            whileInView={{
+                opacity: 1,
+            y: 0
+            }}
+            viewport={{ once: true }}
+            className='w-32 h-32 rounded-full xl:w-[20px] xl:h-[200px] object-cover object-center' 
+                src="https://st4.depositphotos.com/21230196/23977/v/450/depositphotos_239776244-stock-illustration-specialist-icon-vector-white-background.jpg"
+                alt="" 
+            />
 
         </article>
     )
