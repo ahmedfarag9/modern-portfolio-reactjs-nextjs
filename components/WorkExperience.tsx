e3125856694ec81ce929849d825438c226b075b1
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-15 01:40:29 +0200

    Add header

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -13,6 +13,12 @@ function WorkExperience({}: Props) {
     whileInView={{opacity: 1}}
     className=' flex flex-col relative h-screen text-center md:text-left
     md:flex-row max-w-7xl px-10 justify-evenly items-center'>
+        <h3
+        className='absolute top-24 uppercase tracking-[20px] text-gray-500
+        text-2xl'>
+        Experience
+        </h3>
+    
 
     </motion.div>
   )
