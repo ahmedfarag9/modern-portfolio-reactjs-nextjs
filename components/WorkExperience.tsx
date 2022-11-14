9aa452b34f1d81d98c7537524695ee09a694d1fb
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-15 01:40:11 +0200

    Add motion div

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -7,7 +7,14 @@ type Props = {}
 
 function WorkExperience({}: Props) {
   return (
+    <motion.div 
+    initial={{ opacity: 0 }}
+    transition={{ duration: 1 }}
+    whileInView={{opacity: 1}}
+    className=' flex flex-col relative h-screen text-center md:text-left
+    md:flex-row max-w-7xl px-10 justify-evenly items-center'>
 
+    </motion.div>
   )
 }
 
