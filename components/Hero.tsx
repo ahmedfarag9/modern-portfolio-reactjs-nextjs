110de059934be88b5471c57da83889889037ad4d
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-11-14 23:04:05 +0200

    Edit dev style

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,8 +29,10 @@ export default function Hero({}: Props) {
         alt='user profile picture'
       />
 
+      <div className='z-20'>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer</h2>
 
-      <h1>
+        <h1 className='text-3xl lg:text-6xl font-semibold px-10'>
           <span>{text}</span>
           <Cursor cursorColor='#F7AB0A'/>
         </h1>
