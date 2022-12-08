91166339dc6c6de095221b449b6083608fb26f10
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-12-08 08:48:44 +0200

    Add new headings

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -25,10 +25,10 @@ export default function ExperienceCard({}: Props) {
             />
 
             <div className='px-0 md:px-10'>
-                <h4 className='uppercase text-4xl font-light'>IT Specialist</h4>
-                <p className='uppercase font-bold text-2xl mt-1'>The Egyptian Armed Forces</p>
+                <h4 className='uppercase text-3xl font-light'>IT Specialist</h4>
+                <p className='uppercase font-bold text-xl mt-1'>Company X</p>
                 <div className='flex space-x-2 my-2'>
-                    <img 
+                    {/* <img 
                     className='h-10 w-10 rounded-full'
                     src="https://st4.depositphotos.com/21230196/23977/v/450/depositphotos_239776244-stock-illustration-specialist-icon-vector-white-background.jpg"
                     alt=""
@@ -42,12 +42,10 @@ export default function ExperienceCard({}: Props) {
                     className='h-10 w-10 rounded-full'
                     src="https://st4.depositphotos.com/21230196/23977/v/450/depositphotos_239776244-stock-illustration-specialist-icon-vector-white-background.jpg"
                     alt=""
-                    />                                        
+                    />                                         */}
                 </div>
                 <p className='uppercase py-5 text-gray-300'>Started work.. - Ended..</p>
-                <ul className='list-disc space-y-4 text-lg'>
-                    <li>Summary points Summary points Summary points</li>
-                    <li>Summary points Summary points Summary points</li>
+                <ul className='list-disc space-y-4 text-sm md:text-xl'>
                     <li>Summary points Summary points Summary points</li>
                     <li>Summary points Summary points Summary points</li>
                     <li>Summary points Summary points Summary points</li>
