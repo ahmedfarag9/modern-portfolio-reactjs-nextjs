f35415f0dd22919b37eda3f306d30b960cb73fed
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-12-08 08:46:13 +0200

    Change style

--- components/About.tsx
+++ components/About.tsx
@@ -11,9 +11,9 @@ function About({}: Props) {
     initial={{ opacity: 0 }}
     transition={{ duration: 1 }}
     whileInView={{opacity: 1}}
-    className=' flex flex-col relative h-screen text-center md:text-left
-    md:flex-row max-w-7xl px-10 justify-evenly items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500
+    className=' h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-full
+    px-10 justify-evenly mx-auto items-center'>
+        <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500
         text-2xl'>
             About
         </h3>
