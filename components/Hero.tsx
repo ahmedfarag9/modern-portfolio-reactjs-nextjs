0128989429c5735683550e303d5afd063ebee709
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-12-08 08:52:00 +0200

    Edit dev style

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ type Props = {}
 export default function Hero({}: Props) {
   const [text, count] = useTypewriter({
     words: [
-      'Hi, The Name is Ahmed Farag',
+      'Hi, The Name is Ahmed',
       'Guy-who-loves-Reading.tsx',
       '<ButLovesToCodeMore/>'
     ],
@@ -31,14 +31,14 @@ export default function Hero({}: Props) {
       />
       
       <div className='z-20'>
-        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer</h2>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[11px]'>Software Engineer</h2>
 
-        <h1 className='text-3xl lg:text-6xl font-semibold px-10'>
+        <h1 className='text-xl lg:text-6xl font-semibold px-10'>
             <span>{text}</span>
             <Cursor cursorColor='#F7AB0A'/>
         </h1>
 
-        <div className='pt-5'>
+        <div className='pt-5 '>
           <Link href='#about'>
             <button className='heroButton'>About</button>
           </Link>
