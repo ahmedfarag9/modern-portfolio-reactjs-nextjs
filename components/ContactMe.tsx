521f772ad48db4b6fca0c9d68a1ffb0716afb6cb
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-12-11 11:48:29 +0200

    Edit dev style

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -26,18 +26,19 @@ function ContactMe({}: Props) {
   return (
     <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl
     px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500
-        text-2xl'>
+        
+            <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500
+            text-2xl '>
             Contact
         </h3>
 
-        <div className='flex flex-col space-y-10'>
-            <h4 className='text-4xl font-semibold text-center'>
+        <div className='flex flex-col space-y-6 '>
+            {/* <h4 className='text-4xl font-semibold text-center'>
                 I have got just what you need.{" "}
                 <span className='underline decoration-[#F&AB01]/50'>Let's Talk.</span>
-            </h4>
+            </h4> */}
 
-            <div className='space-y-10'>
+            <div className='space-y-8 mt-10 md:mt-20 l:mt-20 xl:mt-20 '>
                 <div className='flex items-center space-x-5 justify-center'>
                     <PhoneIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse'/>
                     <p className='text-2xl'>+1234567890</p>
