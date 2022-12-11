fc0d1f4b9588bd992605f210d6915c041445846a
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-12-11 11:51:45 +0200

    Edit dev style

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from "framer-motion"
+import Link from 'next/link'
 
 type Props = {}
 
@@ -8,13 +9,10 @@ function Projects({}: Props) {
   const projects = [1, 2, 3, 4, 5];
 
   return (
-    <motion.div
-    initial={{ opacity: 0 }}
-    transition={{ duration: 1.5 }}
-    whileInView={{opacity: 1}}
-    className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row
-    max-w-full justify-evenly mx-auto items-center z-0'>
-      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500
+<div className="h-screen relative flex overflow-hidden flex-col text-left
+    max-w-full justify-evenly mx-auto items-center z-0 pr-3">
+
+  <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500
       text-2xl'>
         Projects
       </h3>
