8796562cba26b2148f82d46fff572fd33894ce84
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-12-11 11:50:12 +0200

    Code refactoring

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -9,17 +9,17 @@ function Skill({ directionLeft }: Props) {
   return (
     <div className='group relative flex cursor-pointer md'>
         <motion.img
-        initial={{
-            x: directionLeft ? -200 : 200,
-            opacity: 0
-        }}
-        transition={{ duration: 1 }}
-        whileInView={{
-            opacity: 1,
-            x: 0
-        }}
-        viewport={{once: true}}
-        src='https://st4.depositphotos.com/21230196/23977/v/450/depositphotos_239776244-stock-illustration-specialist-icon-vector-white-background.jpg'
+        // initial={{
+        //     x: directionLeft ? -200 : 200,
+        //     opacity: 0
+        // }}
+        // transition={{ duration: 1 }}
+        // whileInView={{
+        //     opacity: 1,
+        //     x: 0
+        // }}
+        // viewport={{once: true}}
+        src='https://cdn.worldvectorlogo.com/logos/react-1.svg'
         className='rounded-full border border-grey-500 object-cover w-16 h-16 md:h-20 md:w-20 xl:w-20 xl:h-20
         filter group-hover:grayscale transition duration-300 ease-in-out'
         />
