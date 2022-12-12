cbca7e25224a6b0b154844ba57ed2b52068d60c6
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-12-12 06:44:42 +0200

    Add list elements

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -15,7 +15,7 @@ function Projects({}: Props) {
   <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500
       text-2xl'>
         Projects
-      </h3>
+  </h3>
 
   <div className='container'>
 
@@ -39,12 +39,42 @@ function Projects({}: Props) {
         <div className="card-content">
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab repudiandae magnam harum natus fuga et repellat in maiores.</p>
         </div>
-            </div>
+      </div>
       <div className="card-link-wrapper">
         <a href="" className="card-link">Read More</a>
       </div>
     </li>
-          </div>
+    <li className="card">
+      <div>
+        <h3 className="card-title">Project 3</h3>
+        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
+        <div className="card-content">
+          <p>Phasellus ultrices lorem vel bibendum ultricies. In hendrerit nulla a ante dapibus pulvinar eu eget quam.</p>
+        </div>
+      </div>
+      <div className="card-link-wrapper">
+        <a href="" className="card-link">Read More</a>
+      </div>
+    </li>
+    <li className="card">
+      <div>
+        <h3 className="card-title">Project 4</h3>
+        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
+        <div className="card-content">
+          <p>Aenean posuere mauris quam, pellentesque auctor mi bibendum nec. Sed scelerisque lacus nisi, quis auctor lorem ornare vel.</p>
+        </div>
+      </div>
+      <div className="card-link-wrapper">
+        <a href="" className="card-link">Read More</a>
+      </div>
+    </li>
+    <li className="card">
+      <div>
+        <h3 className="card-title">Project 5</h3>
+        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
+        <div className="card-content">
+          <p>Vestibulum pharetra fringilla felis sit amet tempor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras et arcu sit amet est consequat feugiat. Nam ut sapien pulvinar.</p>
+        </div>
       </div>
       <div className="card-link-wrapper">
         <a href="" className="card-link">Read More</a>
