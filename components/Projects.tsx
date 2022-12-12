960c3d36f8ad5fbd07381349fc1a34ab3b7ef898
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-12-12 06:42:46 +0200

    Add list element

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -17,29 +17,21 @@ function Projects({}: Props) {
         Projects
       </h3>
 
-      <div className='relative w-full flex overflow-scroll overflow-y-hidden snap-x snap-mandatory
-      z-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80
-      '>
-        {projects.map((project, i) => (
-          <div key={i} className='w-screen flex-shrink-0 snap-center flex-col space-y-5 items-center
-          justify-center p-20 md:p-44 h-screen'>
-          <motion.img
-            initial={{ y: -300, opacity: 0 }}
-            transition={{ duration: 1.2 }}
-            whileInView={{opacity: 1, y: 0}}
-            viewport={{ once: true }}
-            src="https://st4.depositphotos.com/21230196/23977/v/450/depositphotos_239776244-stock-illustration-specialist-icon-vector-white-background.jpg"
-            alt="" 
-            />
-            <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
-              <h4 className='text-4xl font-semibold text-center'>
-                <span className='underline decoration-[#F&AB01]/50'> Case Study {i+1} of {projects.length}:</span>   UPS clone
-              </h4>
+  <div className='container'>
 
-              <p className='text-lg text-center md:text-left'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+  <ul className="cards">
+    <li className="card">
+      <div>
+        <h3 className="card-title">Project 1</h3>
+        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt=""/>
+        <div className="card-content">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
+      <div className="card-link-wrapper">
+        <a href="" className="card-link">Read More</a>
+      </div>
+    </li>
             </div>
           </div>
         ))}
